bcc181b772f1bbf53f1c42294ca847c6922698a3
Matthew Witter <matt@matthews-mbp.home>
2019-10-07 14:29:40 -0400

    I'm definitely getting the hang of these

--- logFilter.js
+++ logFilter.js
@@ -17,7 +17,7 @@ function filterLogsOnDate(logs, dateFilter) {
   logs.forEach((log) => {
     const timestampDate = new Date(log.timestamp)
 
-    if (parsedDateFilter.toDateString() === timestampDate.toDateString()) {
+    if (parsedDateFilter.getUTCMonth() === timestampDate.getUTCMonth() && parsedDateFilter.getUTCDate() === timestampDate.getUTCDate() && parsedDateFilter.getUTCFullYear() === timestampDate.getUTCFullYear()) {
       filteredLogs.push(log)
     }
   })
